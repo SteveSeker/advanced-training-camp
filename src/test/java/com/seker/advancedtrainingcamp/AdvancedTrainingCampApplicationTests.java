32009a916112c96abaa70eaf0704eae88e078a9a
steveseker <520dnh3344_>
2021-07-05 22:55:51 +0800

    init

---
+++ src/test/java/com/seker/advancedtrainingcamp/AdvancedTrainingCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seker.advancedtrainingcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedTrainingCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
