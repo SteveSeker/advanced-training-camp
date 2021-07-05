c79ff6b1ac90e8232f2468394f2bd25fd871998b
steveseker <520dnh3344_>
2021-07-05 22:48:09 +0800

    init

---
+++ src/main/java/com/seker/advancedtrainingcamp/AdvancedTrainingCampApplication.java
@@ -0,0 +1,13 @@
+package com.seker.advancedtrainingcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedTrainingCampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedTrainingCampApplication.class, args);
+    }
+
+}
