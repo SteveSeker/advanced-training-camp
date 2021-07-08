2c854c6ffb8ce50f15bfdbb4e05d26d26ddbb109
steveseker <520dnh3344_>
2021-07-08 08:46:49 +0800

    20210708

---
+++ src/main/java/com/seker/advancedtrainingcamp/java20210708/LocalVariableTest.java
@@ -0,0 +1,14 @@
+package com.seker.advancedtrainingcamp.java20210708;
+
+public class LocalVariableTest {
+
+    public static void main(String[] args) {
+        MovingAverage ma = new MovingAverage();
+        int num1 = 1;
+        int num2 = 2;
+        ma.submit(num1);
+        ma.submit(num2);
+        double avg = ma.getAvg();
+    }
+
+}
